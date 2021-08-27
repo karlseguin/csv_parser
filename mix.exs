6495ae3fe8bb47308704410c7c30a92573c76b8d
Karl Seguin <k@openmymind.io>
2021-08-27 14:09:36 +0800

    re-add erlsom dep

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule CsvParser.MixProject do
 		[
 			app: :csv_parser,
 			deps: deps(),
-			version: "0.1.0",
+			version: "0.1.1",
 			elixir: "~> 1.11",
 			elixirc_paths: paths(Mix.env),
 			build_embedded: Mix.env == :prod,
@@ -28,7 +28,7 @@ defmodule CsvParser.MixProject do
 		[
 			{:csv, "~> 2.4.1"},
 			{:erlsom, "~> 1.5.0"},
-			{:ex_doc, ">= 0.25.1", only: :dev, runtime: false}
+			{:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
 		]
 	end
 end
